0c7413b81e5ece66d32a89cfa82736e7948ff349
BinhAn14 <an100277@donga.edu.vn>
2024-10-08 23:23:05 +0700

    ip

---
+++ gkiweb/gkiweb/src/test/java/com/example/gkiweb/GkiwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gkiweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkiwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
