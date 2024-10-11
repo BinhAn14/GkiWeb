138ee078104e85ac8f21b71ccf671f35a7e109e5
BinhAn14 <an100277@donga.edu.vn>
2024-10-11 13:47:14 +0700

    ip

--- gkiweb/src/main/java/com/example/gkiweb/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.gkiweb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.gkiweb.model.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
